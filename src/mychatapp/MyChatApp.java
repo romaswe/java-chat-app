5716b1af79dd9de0164e254d95412316352a5a30
Sebastian F <forsthsebbe@hotmail.com>
2017-03-01 20:39:16 +0100

    chat app

---
+++ src/mychatapp/MyChatApp.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mychatapp;
+
+import mychatapp.gui.MainScreen;
+
+/**
+ *
+ * @author romaswe
+ */
+public class MyChatApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainScreen screen = new MainScreen();
+        screen.show();
+    }
+    
+}
